0c9408bfb36b2195213859ce1fe562fa7c4cb0c2
Kasheftin <Kasheftin@gmail.com>
2017-10-06 15:50:53 +0300

    second alg

--- src/components/RulesTest.vue
+++ src/components/RulesTest.vue
@@ -1,20 +1,20 @@
 <template>
   <div class="container">
     <h3>Timeblock Rules Test</h3>
-    <div v-for="type in rows" class="card mb-3">
+    <div class="card mb-3">
       <div class="card-body">
-        <h5 class="card-title">{{type.id}}</h5>
+        <h5 class="card-title">Day Of Week</h5>
         <div class="bar">
           <div class="bar-grid">
             <div v-for="i in total_weeks"></div>
           </div>
           <transition-group class="bar-content" name="slide" mode="out-in">
-            <div v-for="rule in type.rules" class="bar-rule" :style="getRuleStyle(rule)" :key="rule.id" :class="{'-highlighted': rule.highlighted}">
-              <div class="bar-rule-inner">{{rule.note}}</div>
+            <div v-for="rule in rules" class="bar-rule" :style="getRuleStyle(rule)" :key="rule.id">
+              <div class="bar-rule-inner">{{rule.value}}</div>
             </div>
           </transition-group>
           <transition name="slide">
-            <div v-if="showPreliminary && form.type === type.id" class="bar-rule -preliminary" :style="getRuleStyle(form)">
+            <div v-if="showPreliminary" class="bar-rule -preliminary" :style="getRuleStyle(form)">
               <div class="bar-rule-inner"></div>
             </div>
           </transition>
@@ -44,49 +44,13 @@
             <div class="w-100"></div>
             <div class="col">
               <div class="form-group">
-                <label for="formRuleType">Rule Type</label>
-                <select class="form-control" id="formRuleType" v-model="form.type" :readonly="loading">
-                  <option v-for="type in types" :value="type.id">{{type.id}}</option>
-                </select>
-              </div>
-            </div>
-            <div class="col" v-if="form.type == 'day_of_week'">
-              <div class="form-group">
                 <label for="formDay">Day of week</label>
-                <select class="form-control" id="formDay" v-model="form.value.day" :readonly="loading">
+                <select class="form-control" id="formDay" v-model="form.value" :readonly="loading">
                   <option value="MON">MON</option>
                   <option value="TUE">TUE</option>
                   <option value="WED">WED</option>
                   <option value="THU">THU</option>
                   <option value="FRI">FRI</option>
-                </select>
-              </div>
-            </div>
-            <div class="col" v-if="form.type === 'time_of_day'">
-              <div class="form-group">
-                <label for="formTime">Start Time</label>
-                <input type="text" class="form-control" id="formTime" v-model="form.value.time" :readonly="loading">
-              </div>
-            </div>
-            <div class="col" v-if="form.type === 'time_of_day'">
-              <div class="form-group">
-                <label for="formDuration">Duration</label>
-                <input type="text" class="form-control" id="formDuration" v-model="form.value.duration" :readonly="loading">
-              </div>
-            </div>
-            <div class="col" v-if="form.type === 'status'">
-              <div class="form-group">
-                <label for="formStatus">Status</label>
-                <select class="form-control" id="formStatus" v-model="form.value.status_id" :readonly="loading">
-                  <option v-for="status in statuses" :value="status.id">{{status.name}}</option>
-                </select>
-              </div>
-            </div>
-            <div class="col" v-if="form.type === 'location'">
-              <div class="form-group">
-                <label for="formStatus">Location</label>
-                <select class="form-control" id="formLocation" v-model="form.value.location_id" :readonly="loading">
-                  <option v-for="location in locations" :value="location.id">{{location.name}}</option>
                 </select>
               </div>
             </div>
@@ -105,48 +69,20 @@
 </template>
 
 <script>
-// import moment from 'moment'
 
 const formDefaultValues = {
   start_week: 0,
   end_week: undefined,
-  type: undefined,
-  value: {
-    day: undefined,
-    time: undefined,
-    duration: undefined,
-    status_id: undefined,
-    location_id: undefined
-  },
-  highlighted: false
+  value: 'MON'
 }
 
 export default {
   data () {
     return {
       total_weeks: 50,
-      types: [
-        { id: 'day_of_week' },
-        { id: 'time_of_day' },
-        { id: 'status' },
-        { id: 'location' }
-      ],
-      statuses: [
-        { id: 1, name: 'Active' },
-        { id: 2, name: 'Passive' },
-        { id: 3, name: 'One More' }
-      ],
-      locations: [
-        { id: 1, name: 'Room 1' },
-        { id: 2, name: 'Room 2' },
-        { id: 3, name: 'Room 3' }
-      ],
       rules: [
-        { id: 1, type: 'day_of_week', start_week: 20, end_week: 39, value: { day: 'FRI' }, highlighted: false },
-        { id: 2, type: 'day_of_week', start_week: 40, value: { day: 'WED' }, highlighted: false },
-        { id: 3, type: 'time_of_day', start_week: 10, value: { time: '10:00', duration: 60 }, highlighted: false },
-        { id: 4, type: 'status', start_week: 10, value: { status_id: 1 }, highlighted: false },
-        { id: 5, type: 'location', start_week: 10, value: { location_id: 1 }, highlighted: false }
+        { id: 1, start_week: 20, end_week: 39, value: 'FRI' },
+        { id: 2, start_week: 40, value: 'WED' }
       ],
       autoincrementIndex: 0,
       form: Object.assign({}, formDefaultValues),
@@ -154,37 +90,6 @@ export default {
       loading: false,
       showPreliminary: true,
       notes: []
-    }
-  },
-  computed: {
-    rulesByType () {
-      const rulesByType = {}
-      this.rules.forEach(rule => {
-        rule = Object.assign({}, rule)
-        if (!rulesByType.hasOwnProperty(rule.type)) {
-          rulesByType[rule.type] = []
-        }
-        if (rule.type === 'day_of_week') {
-          rule.note = rule.value.day
-        } else if (rule.type === 'time_of_day') {
-          rule.note = rule.value.time + ' ' + rule.value.duration + 'min'
-        } else if (rule.type === 'status') {
-          rule.note = (this.statuses.find(status => status.id === rule.value.status_id) || {}).name
-        } else if (rule.type === 'location') {
-          rule.note = (this.locations.find(location => location.id === rule.value.location_id) || {}).name
-        } else {
-          rule.note = ''
-        }
-        rulesByType[rule.type].push(rule)
-      })
-      return rulesByType
-    },
-    rows () {
-      const out = []
-      this.types.forEach(type => {
-        out.push(Object.assign({}, type, {rules: this.rulesByType[type.id]}))
-      })
-      return out
     }
   },
   methods: {
@@ -200,53 +105,145 @@ export default {
         width: Math.floor(((rule.end_week === undefined ? (this.total_weeks - 1) : rule.end_week) - rule.start_week + 1) / this.total_weeks * 100) + '%'
       }
     },
-    valuesEqual (rule1, rule2) {
-      if (rule1.type !== rule2.type) return false
-        // Dirty
-      return JSON.stringify(rule1.value) === JSON.stringify(rule2.value)
-    },
     submit () {
-      // Validate
       this.error = undefined
-      let startWeek, endWeek, type
+      let startWeek, endWeek
       try {
         startWeek = Math.floor(this.form.start_week)
         if (isNaN(startWeek) || startWeek < 0 || startWeek > this.total_weeks) throw new Error('Start week field is incorrect')
         if (this.form.end_week !== '' && this.form.end_week !== undefined) {
           endWeek = Math.floor(this.form.end_week)
-          console.log(this.form.end_week)
           if (isNaN(endWeek) || endWeek < 0 || endWeek > this.total_weeks) throw new Error('End week field is incorrect')
         }
         if (startWeek !== undefined && endWeek !== undefined && endWeek < startWeek) throw new Error('End week must be greater than the start week')
-        type = this.form.type
-        if (!this.types.find(t => t.id === type)) throw new Error('Rule type is not supported')
+        this.processNewRule({
+          start_week: startWeek,
+          end_week: endWeek,
+          value: this.form.value
+        })
       } catch (e) {
         this.error = e.message
-        return
       }
-      const value = {}
-      if (type === 'day_of_week') {
-        value.day = this.form.value.day
-      } else if (type === 'time_of_day') {
-        value.time = this.form.value.time
-        value.duration = this.form.value.duration
-      } else if (type === 'status') {
-        value.status_id = this.form.value.status_id
-      } else if (type === 'location') {
-        value.location_id = this.form.value.location_id
-      }
-      const newRule = {
-        type,
-        start_week: startWeek,
-        end_week: endWeek,
-        value
-      }
-      this.processNewRule(newRule)
     },
     reset () {
       this.form = Object.assign({}, formDefaultValues)
       this.error = undefined
     },
+    processNewRule (rule) {
+      const delay = (n) => () => new Promise((resolve, reject) => setTimeout(resolve, n || 1000))
+      console.log('ok, here', rule)
+      Promise.resolve()
+        .then(() => {
+          this.loading = true
+          this.notes = []
+        })
+        .then(delay())
+        .then(() => {
+          const events = []
+          this.rules.forEach(r => {
+            events.push({
+              type: 'up',
+              week: r.start_week,
+              id: r.id,
+              value: r.value
+            })
+            if (r.end_week) {
+              events.push({
+                type: 'down',
+                week: r.end_week,
+                id: r.id,
+                value: r.value
+              })
+            }
+          })
+          events.push({
+            type: 'up',
+            week: rule.start_week,
+            new: true,
+            value: rule.value
+          })
+          if (rule.end_week) {
+            events.push({
+              type: 'down',
+              week: rule.end_week,
+              new: true,
+              value: rule.value
+            })
+          }
+          events.sort((e1, e2) => e1.week - e2.week)
+          console.log('events', events)
+          return events
+        })
+        .then(events => {
+          const out = []
+          const levels = []
+          let newRule
+          events.forEach(event => {
+            if (event.type === 'up') {
+              if (event.new) {
+                newRule = event
+                if (levels.length > 0) {
+                  const e = levels[levels.length - 1]
+                  if (e.week <= event.week - 1) {
+                    out.push({
+                      id: e.id,
+                      start_week: e.week,
+                      value: e.value,
+                      end_week: event.week - 1
+                    })
+                  }
+                }
+              } else {
+                levels.push(event)
+              }
+            } else {
+              if (event.new) {
+                out.push({
+                  new: true,
+                  start_week: newRule.week,
+                  end_week: event.week,
+                  value: event.value
+                })
+                if (levels.length > 0) {
+                  levels[levels.length - 1].week = event.week + 1
+                }
+                newRule = undefined
+              } else {
+                if (levels.length > 0) {
+                  const e = levels.pop()
+                  if (!newRule && e.week <= event.week) {
+                    out.push({
+                      id: e.id,
+                      start_week: e.week,
+                      value: e.value,
+                      end_week: event.week
+                    })
+                  }
+                }
+              }
+            }
+          })
+          if (newRule) {
+            out.push({
+              new: true,
+              start_week: newRule.week,
+              value: newRule.value
+            })
+          } else if (levels.length > 0) {
+            const e = levels.pop()
+            out.push({
+              id: e.id,
+              start_week: e.week,
+              value: e.value
+            })
+          }
+          console.log('Resul: ', out)
+        })
+        .then(() => {
+          this.loading = false
+        })
+    }
+    /*
     processNewRule (rule) {
       let rule1, rule2
       const delay = (n) => () => new Promise((resolve, reject) => setTimeout(resolve, n || 1000))
@@ -389,81 +386,8 @@ export default {
           })
           this.loading = false
         })
-
-      /*
-      console.log('Adding rule', Object.assign({}, rule))
-      // 1. Select all weeks that are covered by the rule and destroy them
-      for (let i = this.rules.length - 1; i >= 0; i--) {
-        const r = this.rules[i]
-        if (r.type === rule.type && r.start_week >= rule.start_week && (rule.end_week === undefined || (rule.end_week !== undefined && r.end_week !== undefined && r.end_week <= rule.end_week))) {
-          console.log('Delete rule = ', Object.assign({}, r))
-          this.rules.splice(i, 1)
-        }
-      }
-      // 2. Select the week that covers rule.start_week
-      let rule1 = this.rules.find(r => r.type === rule.type && r.start_week <= rule.start_week && (r.end_week === undefined || r.end_week >= rule.start_week))
-      // 3. Select the week that covers rule.end_week
-      let rule2
-      if (rule.end_week === undefined) {
-        rule2 = this.rules.find(r => r.type === rule.type && r.end_week === undefined)
-      } else {
-        rule2 = this.rules.find(r => r.type === rule.type && r.start_week <= rule.end_week && (r.end_week === undefined || r.end_week >= rule.end_week))
-      }
-      console.log('found rule1 = ', Object.assign({}, rule1), 'found rule2 = ', Object.assign({}, rule2))
-      // 4. Continue rule1 after the rule
-      if (rule1 && rule.end_week !== undefined) {
-        if (rule1.end_week === undefined || rule1.end_week > rule.end_week) {
-          const clonedRule1 = Object.assign({}, rule1)
-          clonedRule1.id = this.getNextIndex()
-          clonedRule1.start_week = rule.end_week + 1
-          console.log('Clone rule1 to continue after, clonedRule1 = ', Object.assign({}, clonedRule1))
-          this.rules.push(clonedRule1)
-        }
-      }
-      // 5. restrict rule1 to finish before the rule
-      if (rule1) {
-        if (rule1.start_week < rule.start_week) {
-          rule1.end_week = rule.start_week - 1
-          console.log('Restrict rule1 to finish before, set rule1 = ', Object.assign({}, rule1))
-        }
-      }
-      // 6. restrict rule2 to start after the rule
-      if (rule2) {
-        if (!rule1 || rule2 !== rule1) {
-          if (rule.end_week !== undefined && rule2.start_week <= rule.end_week) {
-            rule2.start_week = rule.end_week + 1
-            console.log('Restrict rule2 to start after, rule2 = ', Object.assign({}, rule2))
-          }
-        }
-      }
-      // 7. Create new rule
-      rule.id = this.getNextIndex()
-      this.rules.push(rule)
-      console.log('New rule inserted = ', Object.assign({}, rule))
-      this.$nextTick(() => {
-        console.log('Result rules: ', Object.assign([], this.rules))
-      })
-      // 8. Merge consistent rules
-      this.rules.sort((r1, r2) => r1.start_week - r2.start_week)
-      this.types.forEach(type => {
-        const rules = this.rules.filter(rule => rule.type === type.id)
-        for (let i = 1; i < rules.length; i++) {
-          const prev = rules[i - 1]
-          const curr = rules[i]
-          console.log('Compare', Object.assign({}, prev), Object.assign({}, curr))
-          if (prev.end_week === curr.start_week - 1 && this.valuesEqual(prev, curr)) {
-            prev.end_week = curr.end_week
-            const index = this.rules.findIndex(rule => rule.id === curr.id)
-            if (index === 0 || index > 0) {
-              this.rules.splice(index, 1)
-              rules.splice(i, 1)
-              i--
-            }
-          }
-        }
-      })
-      */
     }
+    */
   }
 }
 </script>
